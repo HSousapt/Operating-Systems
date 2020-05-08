c77926309ebe0d376058e74c21d10e712220983c
HSousapt <HSousa_94@hotmail.com>
2020-05-08 20:23:18 +0100

    more stuff

--- Exercises/6/client.c
+++ Exercises/6/client.c
@@ -18,13 +18,17 @@ int main(int argc, char** argv)
 
 	while((n = read(0, buffer, sizeof(buffer))) > 0)
 	{
-		if(!strcmp(buffer, "quit"))
+		if(!strcmp(buffer, "quit\n"))
 			break;
 		else
+		{
 			write(fd, buffer, n);
+			memset(buffer, 0, sizeof(buffer));
+		}
 	}
 	
 	puts("Cliente terminou!!!");
+	close(fd);
 
 	return 0;
 }
