0600c3a38c4f9b01104247d40271429e6c04fe20
HSousapt <HSousa_94@hotmail.com>
2020-06-10 02:25:30 +0100

    pipes almost working

--- Trabalho/requestHandler.c
+++ Trabalho/requestHandler.c
@@ -13,29 +13,74 @@ ssize_t readln(int f,char* buff)
 	return (r==-1)?-1:n;
 }
 
-int count_pipes(char* string)
+int count_char(char* string, char c)
 {
 	int r = 0;
 	for(int i = 0; i < strlen(string); i++)
-		if(string[i] == '|') r++;
+		if(string[i] == c) r++;
 	return r;
 }
 
 void execute(char *cmds[], int n)
 {
-	for(int i = 0; i < n; i++)
+	int fd[n][2];
+	int pids[n];
+	char* result = malloc(sizeof(char*)*1024);
+	int status = 0;	
+
+	//cria uma lista de pipes
+	for(int i = 0; i < n;i++)
 	{
-		int pid;
-		if(!(pid = fork()))
-			execlp(cmds[i], cmds[i], NULL);
-		else
-			waitpid(pid, NULL, 0);
+		pipe(fd[i]);
+	}
+	int i;
+	for(i = 0; i < n; i++)
+	{
+		pids[i] = fork();
+		if(pids[i] < 0) perror("ERRO NO FORK\n");
+
+		else if(!pids[i])
+		{
+			int n_args = count_char(cmds[i], ' ') + 1;
+			char *cmd = strtok(cmds[i], " ");
+			char *args[n_args];
+			int j = 0;
+			while(cmd != NULL)
+			{
+				args[j++] = cmd;
+				cmd = strtok(NULL, " ");
+			}
+			args[j]=NULL;
+			
+			if(i == 0)
+			{
+				printf("Primeiro %s\n", args[0]);
+				close(fd[i][0]);
+				dup2(fd[i][1], 1);
+				close(fd[i][1]);
+				execvp(args[0], args);
+				_exit(1);
+			}
+			else
+			{
+				printf("%s\n", args[0]);
+				close(fd[i-1][1]);
+				dup2(fd[i-1][0], 0);
+				close(fd[i-1][0]);
+				//close(fd[i][0]);
+				//dup2(fd[i][1], 1);
+				//close(fd[i][1]);
+				execvp(args[0], args);
+				_exit(1);
+			}
+		}
 	}
 }
 
 void parse_execute(Tasks *ts, int id)
 {
-	int n = count_pipes(ts->tasks[id].name)+ 1;
+	int n = count_char(ts->tasks[id].name, '|') + 1;
+	printf("%d\n", n);
 	char *cmds[n];
 	char *chain = strtok(ts->tasks[id].name, "|");
 	int j = 0;
